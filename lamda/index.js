2723ba07f17b8b557af327829b61739498412aa7
Lauracalderon25 <58373524+Lauracalderon25@users.noreply.github.com>
2019-11-30 19:42:36 -0600

    cambio 2

--- lamda/index.js
+++ lamda/index.js
@@ -3,40 +3,46 @@
 // session persistence, api calls, and more.
 const Alexa = require('ask-sdk-core');
 
-const reto = [
-    "Hoy lleva agua en tu termo",
-    "Hoy deja el auto en casa y camina",
-    "Hoy reduce al minimo la impresiones",
-    "Hoy si ves basura en la calle, recogela",
-    "Hoy utiliza una bolsa de tela o canasta para realizar tus compras",
-    "Hoy no compres tu comida en desechables, mejor lleva tus propios recipientes",
 
-];
 const nivel1 = [
     "Hoy bañate en menos de 5 minutos",
     "Hoy recicla",
     "Hoy solo abre la puerta del refrigerador solo cuando sea necesario",
-    "Hoy utiliza el transporte publico",
-    "Hoy separa tus desechos en organico e inorganico",
-    "Hoy aprovecha al maximo la luz natural para realizar tus actividades",
+    "Hoy utiliza el transporte público",
+    "Hoy separa tus desechos en orgánico e inorgánico",
+    "Hoy aprovecha al máximo la luz natural para realizar tus actividades",
+    "Hoy lleva agua en tu termo",
+    "Hoy deja el auto en casa y camina",
 ];
 
 const nivel2 = [
     "Hoy ayuda a limpiar las calles recogiendo la basura que veas cerca de tu casa",
-    "Hoy utiliza pilas recargables ",
-    "Hoy comparte tu auto o utliza una bicicleta para realizar tus activiades",
-    "Hoy desconecta todos los electronicos que no estas utilizando",
-    "Hoy lleva tus celulares viejitos a un centro de reciclaje",
+    "Hoy útiliza pilas recargables ",
+    "Hoy comparte tu auto o útiliza una bicicleta para realizar tus actividades",
+    "Hoy desconecta todos los electrónicos que no estas útilizando",
+    "Hoy lleva tus celúlares viejos a un centro de reciclaje",
+     "Hoy reduce al minimo la impresiones",
+    "Hoy si ves basura en la calle, recógela",
 ];
 
 const nivel3 = [
-    "Hoy evita los productos de limpieza que dañan el medio ambiente",
-    "Hoy usa la basura organica para generar composta",
+    "Hoy évita los productos de limpieza que dañan el medio ambiente",
+    "Hoy usa la basura orgánica para generar composta",
     "Hoy lava tu auto con una cubeta",
-    "Hoy riega el jardin en horas de poco sol para que el agua no se evapore",
+    "Hoy riega el jardín en horas de poco sol para que el agua no se evapore",
     "Hoy cambia todos los focos incandescentes por focos ahorradores",
     "Hoy compra productos con menos envoltorios",
+    "Hoy utiliza una bolsa de tela o canasta para realizar tus compras",
+    "Hoy no compres tu comida en desechables, mejor lleva tus propios recipientes",
 ];
+
+const fallas=[
+"Aún no has desbloqueado este nivel",
+"Perdón, no te escuche bien",
+"Lo siento, intentalo de nuevo",
+"Aún no tenemos este nivel",
+    
+    ];
 
 const animal = [
     "La población de rinocerontes de Sumatra en Malasia quedó en marzo del 2019 oficialmente extinta con la muerte del último ejemplar de esa especie en el país, una hembra de 25 años llamada Imam que padecía cáncer.",
@@ -46,7 +52,7 @@ const animal = [
     "El guacamayo de Spix se hizo famoso por la película de 20th Century Fox “Río” gracias al personaje del encantador loro Blu, que viaja miles de kilómetros en un intento de salvar su especie. Pero un estudio publicado esta semana descubrió que el ave originaria de Brasil ya se ha extinguido en la naturaleza.",
 ];
 
-const climático = [
+const climatico = [
     "A lo largo de los últimos 50 años, las actividades humanas, y en particular la combustión de combustibles fósiles, han liberado cantidades de dióxido de carbono y otros gases de efecto invernadero suficientes para afectar al clima mundial.",
     "Del ecuador a los polos, el clima y la meteorología tienen grandes repercusiones directas e indirectas en la vida humana. Los fenómenos meteorológicos extremos, como las grandes lluvias, las inundaciones o los huracanes como el que arrasó Nueva Orleáns (EE.UU.) en agosto de 2005, ponen en peligro la salud y destruyen propiedades y medios de subsistencia.",
     "Las variaciones meteorológicas intensas a corto plazo también pueden afectar gravemente a la salud, causando estrés térmico o un frío extremo (hipotermia) y provocar el aumento de la mortalidad por enfermedades cardiacas y respiratorias.",
@@ -54,12 +60,30 @@ const climático = [
     "La elevación del nivel del mar, otra consecuencia del calentamiento global, aumenta el riesgo de inundación de las costas y podría causar desplazamientos de población.",
 ];
 
+const extinto = [
+    "El oso polar,la razón principal del peligro que corren,es el deshielo del Ártico o Polo Norte, su hábitat natural.",
+    "Rinoceron de Java, la razón principal del peligro es la caza, para después convertirse en adorno o la creencia, en China y en culturas orientales, de que su cuerno tiene propiedades curativas. ",
+    "Los tigres, la razón principal del peligro es la caza, aunque también influye la deforestación, una consecuencia indirecta de la acción del ser humano.",
+    "El canguro, la razón principal del peligro es la caza, este marsupial sufre sobre todo los efectos del calentamiento global.",
+    "El atun rojo, la razón principal del peligro, es la utilización del atún rojo en el sushi de calidad del mercado. En estos momentos, ya existe una prohibición temporal para dejar de consumirlo de manera tan abusiva; de lo contrario, podría desaparecer.",
+    "El gorila de montaña, las razones principales del peligro,  es la caza y el deterioro que ha sufrido su hábitat.",
+    "Los pinguinos, la razón principal es que sufren las consecuencias del deshielo y el calentamiento global.",
+    "El perro salvaje africano, las razones principales del peligro, son por la presión tanto por la destrucción de su hábitat, como su caza ilegal por sus pieles y su captura vivos como mascota, pues en algunas zonas de África se han llegado a tener domesticados para su uso como guardianes, igual que ha sucedido con las hienas o, incluso, guepardos.",
+    ];
+    const respuesta = [
+        "¿Quieres saber otro reto?, Sera divertido.",
+        "¿Te gustaría hacer otro reto?,",
+        "¿Quieres saber acerca de animales extinto? Dime, quiero saber de animales extintos",
+        "¿Quieres saber acerca del cambio climático? Dime, quiero saber sobre el cambio climático",
+        "¿Quieres saber acerca de animales en peligro de extinción? Dime, quiero saber sobre animales en peligro de extinción",
+        ];
+
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Hola, bienvenido al reto del dia. En esta aplicación, pódras ayudar al medio ambiente, a través de retos.Los retos son de niveles esta el nivel 1,2 y 3.¿Qué nivel deseas?. O también puedo brindarte noticias acerca del cambio climatico';
+        const speakOutput = 'Hola, bienvenido al reto del dia. En esta aplicación, pódras ayudar al medio ambiente, a través de retos. Los retos son de niveles, está el nivel 1, 2 y 3. ¿Qué nivel deseas?. O también puedo brindarte noticias acerca del cambio climático y animales extintos o en peligro de extincion';
         const speakReprompt = " ¿Aceptas o tienes miedo?";
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -67,47 +91,104 @@ const LaunchRequestHandler = {
             .getResponse();
     }
 };
-const retodeldiaIntentHandler = {
+
+        
+const animalIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'retodeldia';
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'animal';
     },
     handle(handlerInput) {
-        // const speakOutput = reto[Math.floor(Math.random()*reto.length)];
-        //const speakOutput = animal[1];
-        //const speakReprompt = "¿Aceptas el reto?";
-        //const speakOutput = reto[Math.floor(Math.random()*reto.length)];
-        //const speakOutput = animal[1];
-        const request = handlerInput.requestEnvelope.request;
-        var nivel = request.intent.slots.nivel.value;
-        let speechOutput = '';
-        switch (nivel) {
-            case "uno": //nivel 1
-                speechOutput = nivel1[Math.floor(Math.random() * nivel1.length)];
-                break;
-            case "dos": //nivel 2
-
-                speechOutput = nivel2[Math.floor(Math.random() * nivel2.length)];
-                break;
-
-            case "tres": //nivel 3
-                speechOutput = nivel3[Math.floor(Math.random() * nivel3.length)];
-                break;
-
-            default:
-                speechOutput = 'Aun no tienes desbloqueado este nivel';
-                break;
-        }
-        return handlerInput.responseBuilder
-            .speak(`${speechOutput} para saber otro`)
-            .reprompt('puedes decir dime otro para saber otro dato curioso')
-            //.speak(speakOutput)
-            //.reprompt(speakReprompt)
-            .getResponse();
-    }
+     const speechOutput= animal[Math.floor(Math.random()*animal.length)];
+        //var speechOutput= animal[1];
+   
+    
+    return handlerInput.responseBuilder
+     .speak(speechOutput + '. Nosotros somos la causa de la muerte de los animales, ya que no creamos conciencia, ' + respuesta[Math.floor(Math.random()*respuesta.length)])
+    .reprompt('no te escuche bien')
+    .getResponse(); 
 }
+};
 
+const climaticoIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'climatico';
+    },
+    handle(handlerInput) {
+     const speechOutput= climatico[Math.floor(Math.random()*climatico.length)];
+        //var speechOutput= climatico[1];
+   
+    
+    return handlerInput.responseBuilder
+ .speak(speechOutput + '. Estas conciente de esta problematica?, porque esto podria acabar con nuestro planeta, ' + respuesta[Math.floor(Math.random()*respuesta.length)])
+    .reprompt('no te escuche bien')
+    .getResponse(); 
+}
+};
 
+ const extintoIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'extinto';
+    },
+    handle(handlerInput) {
+     const speechOutput= extinto[Math.floor(Math.random()*extinto.length)];
+        //var speechOutput= extinto[1];
+   
+    return handlerInput.responseBuilder
+ .speak(speechOutput + '. Cuida a los animales porque ellos tambien tienen sentimientos, ' + respuesta[Math.floor(Math.random()*respuesta.length)])
+    .reprompt('no te escuche bien')
+    .getResponse(); 
+}
+};   
+
+const nivel1IntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'niveluno';
+    },
+    handle(handlerInput) {
+     const speechOutput= nivel1[Math.floor(Math.random()*nivel1.length)];
+        //var speechOutput= nivel1[1];
+   
+    return handlerInput.responseBuilder
+   .speak(speechOutput + '. No olvides hacer tu reto, por que así ayudas al medio ambiente, ' + respuesta[Math.floor(Math.random()*respuesta.length)])
+    .reprompt('no te escuche bien')
+    .getResponse(); 
+}
+};   
+const nivel2IntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'niveldos';
+    },
+    handle(handlerInput) {
+     const speechOutput= nivel2[Math.floor(Math.random()*nivel2.length)];
+        //var speechOutput= nivel2[1];
+   
+    return handlerInput.responseBuilder
+    .speak(speechOutput + '. No olvides hacer tu reto, por que así ayudas al medio ambiente, ' + respuesta[Math.floor(Math.random()*respuesta.length)])
+    .reprompt('no te escuche bien')
+    .getResponse(); 
+}
+};   
+
+const nivel3IntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'niveltres';
+    },
+    handle(handlerInput) {
+     const speechOutput= nivel3[Math.floor(Math.random()*nivel3.length)];
+        //var speechOutput= nivel3[1];
+   
+    return handlerInput.responseBuilder
+    .speak(speechOutput + '. No olvides hacer tu reto, por que así ayudas al medio ambiente, ' + respuesta[Math.floor(Math.random()*respuesta.length)])
+    .reprompt('no te escuche bien')
+    .getResponse(); 
+}
+};  
 const HelloWorldIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -127,7 +208,7 @@ const HelpIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'You can say hello to me! How can I help?';
+        const speakOutput = 'Me puedes decir aiudaaa';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -142,7 +223,7 @@ const CancelAndStopIntentHandler = {
                 || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
     },
     handle(handlerInput) {
-        const speakOutput = 'Goodbye!';
+        const speakOutput = 'Adios, te me cuidas!';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .getResponse();
@@ -205,7 +286,12 @@ exports.handler = Alexa.SkillBuilders.custom()
         HelpIntentHandler,
         CancelAndStopIntentHandler,
         SessionEndedRequestHandler,
-        retodeldiaIntentHandler,
+        animalIntentHandler,
+        climaticoIntentHandler,
+        extintoIntentHandler,
+        nivel1IntentHandler,
+        nivel2IntentHandler,
+        nivel3IntentHandler,
         IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
     )
     .addErrorHandlers(
